6dd75f895add7404c63c6310d9c99d1de4e2b9a7
edy477 <edy47@mesoamericana.edu.gt>
2020-02-17 18:13:13 -0600

    newchanges

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
+import {BrowserRouter} from 'react-router-dom';
 import  HomePage from './HomePage/HomePage';
 import './App.css';
 
 function App() {
   return (
-
+<BrowserRouter>
     <div className="App">
       <HomePage/>
     </div>
+    </BrowserRouter>
   );
 }
 
--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
-
+import {Link,BrowserRouter} from "react-router-dom";
+import CardProject from "./CarProject";
 const HomePage = ()=> {
     const [title,setTitle] = useState("");
     const [name,setName] = useState("");
@@ -9,9 +10,29 @@ const HomePage = ()=> {
 
     return(
 
+        <div className="HomePage">
+            <nav>
 
-        <h1>{name}</h1>
 
+            <ul >
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About Me</Link>
+                </li>
+                <li>
+                    <Link to="/dashboard">Contact</Link>
+                </li>
+            </ul>
+            </nav>
+        <div className="myName">
+            <h1>{name}</h1>
+            <p>my name is Jose Eduardo Gomez Perez</p>
+            <CardProject/>
+
+        </div>
+        </div>
     );
 }
 
